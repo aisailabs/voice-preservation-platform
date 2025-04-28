4a4094d7105c8b3a1404fdaf7b028caf09498100
AISAI Labs <aisailabs@yahoo.com>
2025-04-28 18:56:22 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+from . import models
+from .database import engine
+from .routers import auth, voice
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.include_router(auth.router)
+app.include_router(voice.router)
+
+@app.get("/")
+def root():
+    return {"message": "Voice Preservation Backend is Up!"}
 
