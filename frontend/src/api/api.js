1178c1b373bad233f9c1221f0c2632a8de5b51cd
AISAI Labs <aisailabs@yahoo.com>
2025-04-28 19:12:12 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,1 +1,17 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:8000',
+});
+
+// Set token automatically if available
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+  return req;
+});
+
+export default API;
 
