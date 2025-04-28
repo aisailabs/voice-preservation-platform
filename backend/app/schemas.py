782d6834d61ed77bc34a04cb15cc6af08c81d2fd
AISAI Labs <aisailabs@yahoo.com>
2025-04-28 18:58:13 +0530

    Update schemas.py

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,1 +1,18 @@
+from pydantic import BaseModel, EmailStr
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class VoiceUploadResponse(BaseModel):
+    message: str
+    file_url: str
 
