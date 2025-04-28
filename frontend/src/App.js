01dd5be2ae34bd42a4b1fa6587c41caf5aa6865c
AISAI Labs <aisailabs@yahoo.com>
2025-04-28 19:13:56 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
+import UploadVoice from './components/UploadVoice';
+
+function App() {
+  return (
+    <div>
+      <h1>Voice Preservation Platform</h1>
+      <Register />
+      <Login />
+      <UploadVoice />
+    </div>
+  );
+}
+
+export default App;
 
